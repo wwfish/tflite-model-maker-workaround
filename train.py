8422a08a3fd51c8f611d38336421a3c0ad375c77
wwfish <30609898+wwfish@users.noreply.github.com>
2023-07-06 12:22:53 +0930

    Update train.py

--- train.py
+++ train.py
@@ -23,6 +23,11 @@ tf.get_logger().setLevel('ERROR')
 from absl import logging
 logging.set_verbosity(logging.ERROR)
 
+# Confirm TF Version
+print("\nTensorflow Version:")
+print(tf.__version__)
+print()
+
 # Load Dataset
 train_data = object_detector.DataLoader.from_pascal_voc(
     'android_figurine/train',
@@ -44,16 +49,26 @@ spec = object_detector.EfficientDetSpec(
   hparams={'max_instances_per_image': 8000})
 
 # Train the model
-model = object_detector.create(train_data, model_spec=spec, batch_size=4, train_whole_model=True, epochs=10, validation_data=val_data)
+model = object_detector.create(train_data, model_spec=spec, batch_size=4, train_whole_model=True, epochs=5, validation_data=val_data)
 
 # Evaluate the model
-model.evaluate(val_data)
-print("Evaluation results:")
-print(evaluation_results)
+eval_result = model.evaluate(val_data)
 
+# Print COCO metrics
+print("COCO metrics:")
+for label, metric_value in eval_result.items():
+    print(f"{label}: {metric_value}")
+
+# Add a line break after all the items have been printed
+print()
 
 # Export the model
 model.export(export_dir='.', tflite_filename='android.tflite')
 
-# Evalute the tflite model
-model.evaluate_tflite('android.tflite', val_data)
+# Evaluate the tflite model
+tflite_eval_result = model.evaluate_tflite('android.tflite', val_data)
+
+# Print COCO metrics for tflite
+print("COCO metrics tflite")
+for label, metric_value in tflite_eval_result.items():
+    print(f"{label}: {metric_value}")
